b9b475aeacbec20d91ded639faf488a1ea450dea
yunver <yunus.unver1995@gmail.com>
2019-06-22 16:17:32 +0300

    Added DbContext, Abstact Dal,Concrete Dal

---
+++ AgainPractice.DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AgainPractice.Core.DataAccess.EntityFramework;
+using AgainPractice.DataAccess.Abstract;
+using AgainPractice.Entities.Concrete;
+
+namespace AgainPractice.DataAccess.Concrete.EntityFramework
+{
+    public class EfProductDal:EfEntityRepositoryBase<Product,PracticeContext>,IProductDal
+    {
+    }
+}
---
+++ AgainPractice.DataAccess/Concrete/EntityFramework/PracticeContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AgainPractice.DataAccess.Concrete.EntityFramework.Mappings;
+using AgainPractice.Entities.Concrete;
+
+namespace AgainPractice.DataAccess.Concrete.EntityFramework
+{
+    public class PracticeContext:DbContext
+    {
+        public PracticeContext()
+        {
+            Database.SetInitializer<PracticeContext>(null);
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new CategoryMap());
+            modelBuilder.Configurations.Add(new ProductMap());
+        }
+    }
+}
