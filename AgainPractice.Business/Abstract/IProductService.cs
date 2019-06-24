3fcbc219fae0a81554b9d7f37d426a365c57cd04
yunver <yunus.unver1995@gmail.com>
2019-06-24 18:22:28 +0300

    Added IProductService and ProductManager

---
+++ AgainPractice.Business/Abstract/IProductService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AgainPractice.Entities.Concrete;
+
+namespace AgainPractice.Business.Abstract
+{
+    public interface IProductService
+    {
+        List<Product> GetAll();
+        Product Get(int id);
+        void Add(Product product);
+        void Update(Product product);
+        void Delete(Product product);
+    }
+}
---
+++ AgainPractice.Business/Concrete/Managers/ProductManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AgainPractice.Business.Abstract;
+using AgainPractice.DataAccess.Abstract;
+using AgainPractice.Entities.Concrete;
+
+namespace AgainPractice.Business.Concrete.Managers
+{
+    public class ProductManager:IProductService
+    {
+        private IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public List<Product> GetAll()
+        {
+            return _productDal.GetList();
+        }
+
+        public Product Get(int id)
+        {
+            return _productDal.Get(x => x.Id == id);
+        }
+
+        public void Add(Product product)
+        {
+            _productDal.Add(product);
+        }
+
+        public void Update(Product product)
+        {
+            _productDal.Update(product);
+        }
+
+        public void Delete(Product product)
+        {
+            _productDal.Delete(product);
+        }
+    }
+}
