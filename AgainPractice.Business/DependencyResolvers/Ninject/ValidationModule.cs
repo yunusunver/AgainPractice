b7883cc3257cc11080541536bc3f0aa3661a947b
yunver <yunus.unver1995@gmail.com>
2019-06-24 18:02:31 +0300

    Added Validation Module(Ninject)

---
+++ AgainPractice.Business/DependencyResolvers/Ninject/ValidationModule.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AgainPractice.Business.ValidationRules.FluentValidation;
+using AgainPractice.Entities.Concrete;
+using FluentValidation;
+using Ninject.Modules;
+
+namespace AgainPractice.Business.DependencyResolvers.Ninject
+{
+    public class ValidationModule:NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IValidator<Category>>().To<CategoryValidator>();
+            Bind<IValidator<Product>>().To<ProductValidator>();
+        }
+    }
+}
