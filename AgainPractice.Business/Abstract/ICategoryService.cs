bcfeee838a7f0848cdbc166e224559d220ab66f9
yunver <yunus.unver1995@gmail.com>
2019-06-24 18:17:44 +0300

    Added ICategoryService and CategoryManager

---
+++ AgainPractice.Business/Abstract/ICategoryService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using AgainPractice.Entities.Concrete;
+
+namespace AgainPractice.Business.Abstract
+{
+    public interface ICategoryService
+    {
+        List<Category> GetAll();
+        Category Get(int id);
+        void Add(Category category);
+        void Update(Category category);
+        void Delete(Category category);
+    }
+}
---
+++ AgainPractice.Business/Concrete/Managers/CategoryManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AgainPractice.Business.Abstract;
+using AgainPractice.DataAccess.Abstract;
+using AgainPractice.Entities.Concrete;
+
+namespace AgainPractice.Business.Concrete.Managers
+{
+    public class CategoryManager:ICategoryService
+    {
+        private ICategoryDal _categoryDal;
+
+        public CategoryManager(ICategoryDal categoryDal)
+        {
+            _categoryDal = categoryDal;
+        }
+
+        public List<Category> GetAll()
+        {
+            return _categoryDal.GetList();
+        }
+
+        public Category Get(int id)
+        {
+            return _categoryDal.Get(x=>x.Id==id);
+        }
+
+        public void Add(Category category)
+        {
+            _categoryDal.Add(category);
+        }
+
+        public void Update(Category category)
+        {
+            _categoryDal.Update(category);
+        }
+
+        public void Delete(Category category)
+        {
+            _categoryDal.Delete(category);
+        }
+    }
+}
