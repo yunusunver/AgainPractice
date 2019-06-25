0d0dea3806bd0143273a2bb5597cff90385e53cd
yunver <yunus.unver1995@gmail.com>
2019-06-26 00:06:17 +0300

    Added NHibernate Implementation

---
+++ AgainPractice.Core/DataAccess/NHibernate/NHibernateHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NHibernate;
+
+namespace AgainPractice.Core.DataAccess.NHibernate
+{
+    public abstract class NHibernateHelper:IDisposable
+    {
+        private ISessionFactory _sessionFactory;
+
+        public virtual ISessionFactory SessionFactory
+        {
+            get
+            {
+              return  _sessionFactory ?? (_sessionFactory = InitializeFactory());
+            }
+        }
+
+        protected abstract ISessionFactory InitializeFactory();
+
+        public virtual ISession OpenSession()
+        {
+            return SessionFactory.OpenSession();
+        }
+
+        public void Dispose()
+        {
+            GC.SuppressFinalize(this);
+        }
+    }
+}
