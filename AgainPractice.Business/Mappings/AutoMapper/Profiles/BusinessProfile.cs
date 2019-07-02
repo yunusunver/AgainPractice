32b4df12795cbf06edeec8b42553463c59b5e7f9
yunver <yunus.unver1995@gmail.com>
2019-07-02 18:01:53 +0300

    Added Business Profile in AutoMapper

---
+++ AgainPractice.Business/Mappings/AutoMapper/Profiles/BusinessProfile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AgainPractice.Entities.Concrete;
+using AutoMapper;
+
+namespace AgainPractice.Business.Mappings.AutoMapper.Profiles
+{
+    public class BusinessProfile:Profile
+    {
+        public BusinessProfile()
+        {
+            CreateMap<Product, Product>();
+        }
+    }
+}
